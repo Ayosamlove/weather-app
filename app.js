a4bb3d7614378165ca7924f3d23bbecbf669879b
Dammy Knight <olaoluwaayomide10@gmail.com>
2023-08-14 02:25:30 +0100

    UI update completed

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 'use strict';
 
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
 
+const updateUI = function (data) {
+	const cityDetails = data.cityDetails;
+	const weather = data.weather;
+
+	//update details template
+	details.innerHTML = `
+        <h5 class="my-3">${cityDetails.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+        <span>${weather.Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+        </div>
+        `;
+
+	// remove d-none class if present
+	if (card.classList.contains('d-none')) {
+		card.classList.remove('d-none');
+	}
+};
 const updateCity = async function (city) {
 	// console.log(city);
 
@@ -9,8 +30,8 @@ const updateCity = async function (city) {
 	const weather = await getWeather(cityDetails.Key);
 
 	return {
-		cityDetails: cityDetails,
-		weather: weather,
+		cityDetails,
+		weather,
 	};
 };
 
@@ -23,6 +44,6 @@ cityForm.addEventListener('submit', (e) => {
 
 	//update UI with new city value
 	updateCity(city)
-		.then((data) => console.log(data))
+		.then((data) => updateUI(data))
 		.catch((err) => console.log(err));
 });
