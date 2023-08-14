486a4144924df08be6f5b214e93d951c3a9e0ce1
Dammy Knight <olaoluwaayomide10@gmail.com>
2023-08-14 01:54:15 +0100

    Updating the location on UI

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const cityForm = document.querySelector('form');
+
+const updateCity = async function (city) {
+	// console.log(city);
+
+	const cityDetails = await getCity(city);
+	const weather = await getWeather(cityDetails.Key);
+
+	return {
+		cityDetails: cityDetails,
+		weather: weather,
+	};
+};
+
+cityForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	//get city value
+	const city = cityForm.city.value.trim();
+	cityForm.reset();
+
+	//update UI with new city value
+	updateCity(city)
+		.then((data) => console.log(data))
+		.catch((err) => console.log(err));
+});
